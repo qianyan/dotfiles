8bf4d0dabbb87891f03de4e993d2f166937d08ff
Yan Qian <qianyan@thoughtworks.com>
2016-10-12 07:04:08 +0800

    [emacs] shorten eshell prompt path.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -189,3 +189,25 @@
   (add-to-list 'default-frame-alist no-border)
   (add-to-list 'initial-frame-alist no-border))
 
+(defun shortened-path (path max-len)
+      "Return a modified version of `path', replacing some components
+      with single characters starting from the left to try and get
+      the path down to `max-len'"
+      (let* ((components (split-string (abbreviate-file-name path) "/"))
+             (len (+ (1- (length components))
+                     (reduce '+ components :key 'length)))
+             (str ""))
+        (while (and (> len max-len)
+                    (cdr components))
+          (setq str (concat str (if (= 0 (length (car components)))
+                                    "/"
+                                  (string (elt (car components) 0) ?/)))
+                len (- len (1- (length (car components))))
+                components (cdr components)))
+        (concat str (reduce (lambda (a b) (concat a "/" b)) components))))
+
+(defun rjs-eshell-prompt-function ()
+  (concat (shortened-path (eshell/pwd) 10)
+          (if (= (user-uid) 0) " # " " $ ")))
+
+(setq eshell-prompt-function 'rjs-eshell-prompt-function)
