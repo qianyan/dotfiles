5e8fe13f5aad9412db10e99b2b20a306f34fbbff
Yan Qian <qianyan@thoughtworks.com>
2019-09-21 20:27:57 +0800

    [dotfile.sh] rewrite dotfile to make .xxx to .xxx->dotfiles/xxx

--- dotfile.sh
+++ dotfile.sh
@@ -1,10 +1,24 @@
+#!/usr/bin/env bash
+
+dotfile_dir=~/dotfiles
 dotfile () {
     set -e
-    echo "dotfile now..."
-    old_file="$1.dotfile"
-    dot_file=~/dotfiles/${1:1}
-    cp -r $1 $old_file
-    mv $old_file $dot_file
-    ln -sF $dot_file $1
-    echo "dotfile migration done."
+    echo "dotfile for [ $1 ] now..."
+    dot_file=$1
+    swap="$1.swap" # swap file saved to dotfiles/
+    file_name=`echo $dot_file | sed -n 's/.*\.\(.*\)/\1/p'` # e.g. /Users/whoami/.vimrc -> vimrc
+    no_dot="$dotfile_dir/$file_name"
+
+    echo "1. swap $dot_file to $swap"
+    cp $dot_file $swap
+
+    echo "2. move $swap to $no_dot"
+    mv $swap $no_dot
+
+    echo "3. replace $no_dot with $dot_file"
+    ln -sF $no_dot $dot_file 
+
+    echo "done [ $dot_file ] -> [ $no_dot ]."
 }
+
+dotfile "$@"
