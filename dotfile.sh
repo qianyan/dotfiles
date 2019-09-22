b39294f8e54fbed9fef483dc8b31227e0c0265ab
Yan Qian <qianyan@thoughtworks.com>
2019-09-22 21:52:47 +0800

    [dotfile.sh] fix dotfile to make .xxx.conf to .xxx.conf->dotfiles/xxx.conf

--- dotfile.sh
+++ dotfile.sh
@@ -6,7 +6,7 @@ dotfile () {
     echo "dotfile for [ $1 ] now..."
     dot_file=$1
     swap="$1.swap" # swap file saved to dotfiles/
-    file_name=`echo $dot_file | sed -n 's/.*\.\(.*\)/\1/p'` # e.g. /Users/whoami/.vimrc -> vimrc
+    file_name=`echo $dot_file | sed -n 's/[^\.]*\.\(.*\)/\1/p'` # e.g. /Users/whoami/.vimrc -> vimrc; test for `[^\.]*` /Users/whoami/.xxx.conf -> xxx.conf
     no_dot="$dotfile_dir/$file_name"
 
     echo "1. swap $dot_file to $swap"
@@ -16,7 +16,7 @@ dotfile () {
     mv $swap $no_dot
 
     echo "3. replace $no_dot with $dot_file"
-    ln -sF $no_dot $dot_file 
+    ln -sf $no_dot $dot_file 
 
     echo "done [ $dot_file ] -> [ $no_dot ]."
 }
