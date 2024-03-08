ce1e76b76065d15045f3618d11d46ac85750307a
qianyan <qianyan@thoughtworks.com>
2024-03-08 20:46:08 +0800

    [emacs] group lambda symbols functions together

--- lisp/init-clojure-cider.el
+++ lisp/init-clojure-cider.el
@@ -15,4 +15,28 @@
              (add-hook 'cider-repl-mode-hook 'company-mode)
              (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode))
 
+(defun my-replace-symbol ()
+  (dolist (mode '(clojure-mode clojurescript-mode cider-mode))
+    (eval-after-load mode
+      (font-lock-add-keywords
+       mode '(
+              ("(\\(fn\\)[\[[:space:]]" ; anon funcs 1
+               (0 (progn (compose-region (match-beginning 1)
+                                         (match-end 1) "λ")
+                         nil)))
+              ("\\(#\\)("               ; anon funcs 2
+               (0 (progn (compose-region (match-beginning 1)
+                                         (match-end 1) "ƒ")
+                         nil)))
+              ("\\(#_\\)("              ; anon funcs 2-1
+               (0 (progn (compose-region (match-beginning 1)
+                                         (match-end 1) "€")
+                         nil)))
+
+              ("\\(#\\){"               ; sets
+               (0 (progn (compose-region (match-beginning 1)
+                                         (match-end 1) "∈")
+                         nil)))
+              )))))
+(add-hook 'after-init-hook 'my-replace-symbol)
 (provide 'init-clojure-cider)
