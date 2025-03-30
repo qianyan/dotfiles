9c095d6cf204d4c436ebabc6c05ea8310a4a35e3
qianyan <qianyan.lambda@gmail.com>
2025-03-30 15:13:00 +0800

    [emacs] add chagpt-shell to chat with deepseek

--- lisp/init-ai-assistant.el
+++ lisp/init-ai-assistant.el
@@ -3,7 +3,7 @@
              :defer t
              :config
              (global-company-mode)
-             (setq company-idle-delay 0)
+             (setq company-idle-delay 1)
              (setq company-show-numbers t))
 
 (use-package company-tabnine
@@ -12,6 +12,7 @@
 
 (add-to-list 'company-backends #'company-tabnine)
 
+;; install `llm` first
 (use-package ellama
   :init
   ;; setup key bindings
@@ -21,35 +22,46 @@
   ;; could be llm-openai for example
   (require 'llm-ollama)
   (setopt ellama-provider
-		    (make-llm-ollama
-		     ;; this model should be pulled to use it
-		     ;; value should be the same as you print in terminal during pull
-		     :chat-model "mistral:latest"
-		     :embedding-model "mistral:latest"))
+	  (make-llm-ollama
+	   ;; this model should be pulled to use it
+	   ;; value should be the same as you print in terminal during pull
+	   :chat-model "mistral:latest"
+	   :embedding-model "mistral:latest"))
   ;; Predefined llm providers for interactive switching.
   ;; You shouldn't add ollama providers here - it can be selected interactively
   ;; without it. It is just example.
   (setopt ellama-providers
-		    '(("gemma:7b" . (make-llm-ollama
-                                     :chat-model "gemma:7b"
-                                     :embedding-model "gemma:7b"))
-                      ("gemma:2b" . (make-llm-ollama
-				     :chat-model "gemma:2b"
-				     :embedding-model "gemma:2b"))
-                      ("codellama" . (make-llm-ollama
-				     :chat-model "codellama"
-				     :embedding-model "codellama"))                      
-		      ("mistral" . (make-llm-ollama
-				    :chat-model "mistral:latest"
-				    :embedding-model "mistral:latest"))
-		      ("mixtral" . (make-llm-ollama
-				    :chat-model "mixtral:8x7b"
-				    :embedding-model "mixtral:8x7b"))))
+	  '(("gemma:7b" . (make-llm-ollama
+                           :chat-model "gemma:7b"
+                           :embedding-model "gemma:7b"))
+            ("gemma:2b" . (make-llm-ollama
+			   :chat-model "gemma:2b"
+			   :embedding-model "gemma:2b"))
+            ("llama3" . (make-llm-ollama
+			 :chat-model "llama3"
+			 :embedding-model "llama3"))                      
+            ("codellama" . (make-llm-ollama
+			    :chat-model "codellama"
+			    :embedding-model "codellama"))                      
+	    ("mistral" . (make-llm-ollama
+			  :chat-model "mistral:latest"
+			  :embedding-model "mistral:latest"))
+	    ("chinese" . (make-llm-ollama
+			  :chat-model "llama2-chinese"
+			  :embedding-model "llama2-chinese"))))
   ;; Naming new sessions with llm
   (setopt ellama-naming-provider
-	    (make-llm-ollama
-	     :chat-model "mistral:latest"
-	     :embedding-model "mistral:latest"))
+	  (make-llm-ollama
+	   :chat-model "mistral:latest"
+	   :embedding-model "mistral:latest"))
   (setopt ellama-naming-scheme 'ellama-generate-name-by-llm))
+
+;;; `pass add api.deepseek.com` save into ~/.password_store/api.deepseek.com.gpg
+(use-package chatgpt-shell
+  :ensure t
+  :custom
+  ((chatgpt-shell-deepseek-key
+    (lambda ()
+      (auth-source-pass-get 'secret "api.deepseek.com")))))
 
 (provide 'init-ai-assistant)
